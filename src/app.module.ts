cca194bbf9827fd537839869d3310e896b444a10
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2023-11-02 19:06:22 -0300

    feat: added cache manager and redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+//import type { RedisClientOptions } from 'redis';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { CacheModule, CacheStore } from '@nestjs/cache-manager';
+
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    //CacheModule.register<RedisClientOptions>({ ~~ can't use type while lib isn't updated
+    //https://github.com/dabroek/node-cache-manager-redis-store/issues/53
+    CacheModule.register({
+      store: redisStore as unknown as CacheStore,
+      host: 'localhost',
+      port: 6379,
+      isGlobal: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
