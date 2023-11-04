6cb0891c02c5c7b5fa643ecd6c9275e2476dd3f5
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2023-11-04 18:21:32 -0300

    feat: added env config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,15 +11,20 @@ import {
 
 import * as redisStore from 'cache-manager-redis-store';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
     //CacheModule.register<RedisClientOptions>({ ~~ can't use type while lib isn't updated
     //https://github.com/dabroek/node-cache-manager-redis-store/issues/53
     CacheModule.register({
       store: redisStore as unknown as CacheStore,
-      host: 'localhost',
-      port: 6379,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
       isGlobal: true,
     }),
     UserModule,
